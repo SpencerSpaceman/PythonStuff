c04b73c1c89d2e5e3964e5de2095067440295ed6
Spencer Laux <spencerlaux3@gmail.com>
2022-05-25 10:26:15 -0400

    cool

---
+++ SpPencer.py
@@ -0,0 +1,14 @@
+from random import randint
+
+num1 = randint(1,10)
+num2 = randint(1,10)
+
+question = "What is " + str(num1) + " times " + str(num2) + "? "
+user_answer = int(input(question))
+correct_answer = num1 * num2
+
+if user_answer == correct_answer:
+	print("Thats right, well done")
+else:
+	print("No, im afraid the answer is " + str(correct_answer)+ ".") 
+ 
